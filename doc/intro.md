1ff3c97964579ebf8b4b43f2659dda3ca0111dab
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2023-10-09 20:34:17 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to promised-queue
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/beetleman/promised_queue_test.clj
@@ -0,0 +1,7 @@
+(ns beetleman.promised-queue-test
+  (:require [clojure.test :refer :all]
+            [beetleman.promised-queue :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
