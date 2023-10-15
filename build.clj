0b5416b62b51b92ccca5f0937c7b873b6e7cf3a4
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2023-10-15 17:50:40 +0200

    update build script

--- build.clj
+++ build.clj
@@ -1,37 +1,47 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.deps :as t]
-            [clojure.tools.build.api :as b]
+  (:require [clojure.tools.build.api :as b]
             [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'net.clojars.beetleman/promised-queue)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
+(def lib 'com.github.beetleman/promised-queue)
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
-  (println "\nRunning tests...")
   (let [basis    (b/create-basis {:aliases [:test]})
-        combined (t/combine-aliases basis [:test])
         cmds     (b/java-command
-                  {:basis basis
-                   :java-opts (:jvm-opts combined)
-                   :main      'clojure.main
-                   :main-args ["-m" "cognitect.test-runner"]})
+                  {:basis      basis
+                    :main      'clojure.main
+                    :main-args ["-m" "cognitect.test-runner"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
 
+(defn- pom-template [version]
+  [[:description "FIXME: my new library."]
+   [:url "https://github.com/beetleman/promised-queue"]
+   [:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "http://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "Beetleman"]]]
+   [:scm
+    [:url "https://github.com/beetleman/promised-queue"]
+    [:connection "scm:git:https://github.com/beetleman/promised-queue.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:beetleman/promised-queue.git"]
+    [:tag (str "v" version)]]])
+
 (defn- jar-opts [opts]
   (assoc opts
-         :lib lib :version version
-         :jar-file (format "target/%s-%s.jar" lib version)
-         :scm {:tag (str "v" version)}
-         :basis (b/create-basis {})
-         :class-dir class-dir
-         :target "target"
-         :src-dirs ["src"]))
+          :lib lib   :version version
+          :jar-file  (format "target/%s-%s.jar" lib version)
+          :basis     (b/create-basis {})
+          :class-dir class-dir
+          :target    "target"
+          :src-dirs  ["src"]
+          :pom-data  (pom-template version)))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (test opts)
@@ -41,7 +51,7 @@
     (b/write-pom opts)
     (println "\nCopying source...")
     (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
-    (println "\nBuilding JAR...")
+    (println "\nBuilding JAR..." (:jar-file opts))
     (b/jar opts))
   opts)
 
